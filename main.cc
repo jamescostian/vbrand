e425c9ad70b410fa8f31e41891c5091eafc5baa1
James Costian <james@jamescostian.com>
2018-05-16 16:55:55 -0500

    Remove silent feature

--- main.cc
+++ main.cc
@@ -7,15 +7,12 @@
 int main(int argc, char **argv) {
   assert(argc == 2);
   string filename = argv[1];
-  bool be_silent = getenv("SILENCE_VBRAND") != NULL;
   if (!file_ends_in(filename, THE_BRAND)) {
     append_to_file(filename, THE_BRAND);
-    if (!be_silent) {
-      cout << "Added brand" << endl;
-    }
+    cout << "Added brand to " << filename << endl;
   }
-  else if (!be_silent) {
-    cout << "Already branded" << endl;
+  else {
+    cout << "Already branded " << filename << endl;
   }
   return 0;
 }
