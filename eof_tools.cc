55bcd9d1661ea9abb78ee7d4e60280f95cf15b92
James Costian <james@jamescostian.com>
2018-05-16 18:32:37 -0500

    Convert std::strings to C-style strings

--- eof_tools.cc
+++ eof_tools.cc
@@ -3,7 +3,7 @@
 #include "eof_tools.h"
 
 bool file_ends_in(string filename, string ending) {
-  ifstream f (filename, ios_base::binary | ios_base::ate);
+  ifstream file (filename.c_str(), ios_base::binary | ios_base::ate);
   assert(f.is_open());
   // Read the end of the file to a buffer and then close the file
   f.seekg(-ending.length(), ios_base::cur);
@@ -13,6 +13,6 @@ bool file_ends_in(string filename, string ending) {
 }
 
 void append_to_file(string filename, string ending) {
-  ofstream f (filename, ios_base::binary | ios_base::app);
+  ofstream file (filename.c_str(), ios_base::binary | ios_base::app);
   f << ending;
 }
