b92d25909fb10ade5cd60bfe9c7bf57ab0318b86
James Costian <james@jamescostian.com>
2018-05-16 17:34:30 -0500

    Remove an unused header

--- eof_tools.h
+++ eof_tools.h
@@ -4,4 +4,3 @@ using namespace std;
 
 bool file_ends_in(string filename, string ending);
 void append_to_file(string filename, string ending);
-void remove_bytes_from_file(string filename, int bytes);
