152da59ddbd4d24e0b7313a8a93a2d871a641861
James Costian <james@jamescostian.com>
2018-05-16 18:45:28 -0500

    Replace an assertion with a USAGE message

--- main.cc
+++ main.cc
@@ -1,10 +1,12 @@
-#include <cassert>
 #include <cstdlib>
 #include <iostream>
 #include "eof_tools.h"
 
 int main(int argc, char **argv) {
-  assert(argc == 2);
+  if (argc != 2) {
+    cerr << "USAGE: vbrand path/to/file" << endl;
+    return 1;
+  }
   string filename = argv[1];
   if (!file_ends_in(filename, THE_BRAND)) {
     append_to_file(filename, THE_BRAND);
