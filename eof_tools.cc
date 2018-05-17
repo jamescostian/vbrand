91c81653c534c05f5f5f8e7f0d79d4310c8f30b2
James Costian <james@jamescostian.com>
2018-05-16 20:38:52 -0500

    uint64_t => int64_t

--- eof_tools.cc
+++ eof_tools.cc
@@ -18,13 +18,13 @@ void append_to_file(string filename, string ending) {
   file << ending;
 }
 
-uint64_t file_size(string filename) {
+int64_t file_size(string filename) {
   struct stat stat_buf;
   stat(filename.c_str(), &stat_buf);
   return stat_buf.st_size;
 }
 
-void remove_bytes_from_file(string filename, uint64_t bytes) {
+void remove_bytes_from_file(string filename, int64_t bytes) {
   string cmd = "truncate -c -s " + to_string(file_size(filename) - bytes) + " " + filename;
   system(cmd.c_str());
 }
--- eof_tools.h
+++ eof_tools.h
@@ -6,4 +6,4 @@ using namespace std;
 
 bool file_ends_in(string filename, string ending);
 void append_to_file(string filename, string ending);
-void remove_bytes_from_file(string filename, uint64_t bytes);
+void remove_bytes_from_file(string filename, int64_t bytes);
